62da90f9cf84259ecaa5f68e5033d6269c2cc902
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-01-20 19:26:57 +0530

    testNavigator

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
-import NavBar from '../../Components/NavBar/NavBar'
+/*import NavBar from '../../Components/NavBar/NavBar'
 import Header from '../../Components/Header/Header'
 import About from '../../Components/About Us/AboutUs'
 import Developer from '../../Components/Game Developer/GameDeveloper'
 import Customer from '../../Components/Customer/Customer'
 import Experience from '../../Components/Experience/Experience'
-import Contact from '../../Components/Contact/Contact'
+import Contact from '../../Components/Contact/Contact'*/
+
+import TestNavigator from '../../Components/TestNavigator/TestNavigator'
 
 const HomePage = () => {
   return (
     <div>
-      <NavBar/>
-      <Header/>
-      <About/>
-      <Developer/>
-      <Customer/>
-      <Experience/>
-      <Contact/>
+     <TestNavigator/>
     </div>
   )
 }
