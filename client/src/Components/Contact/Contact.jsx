e266d2ab38ef4c3392b783222626e5c757e26468
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-02-13 14:01:46 +0530

    contact

--- client/src/Components/Contact/Contact.jsx
+++ client/src/Components/Contact/Contact.jsx
@@ -1,42 +1,72 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineHome } from "react-icons/ai";
+import { FiPhone } from "react-icons/fi"
+
 import "./Contact.css";
 
 const Contact = () => {
   return (
     <section id="contact">
-      <div className="heading">
-        <font className="title">Contact Us</font>
+      <div className="contact-heading">
+        <font className="contact-title">Contact Us</font>
       </div>
 
       <div className="contact-content">
-
         <div className="card-holder">
-        <Card className="contact-card">
-          <Card.Body className="card-body">
-            <div className="contact-icon">
-              gfj
-            </div>
+          <Card className="contact-card">
+            <Card.Body>
+              <div className="card-body">
+                <div className="contact-icon">
+                  <AiOutlineMail />
+                </div>
 
-            <div className="contact-detail">
-            hello@voasiz.com
-            </div>
-
-          </Card.Body>
-        </Card>
+                <div className="contact-detail">hello@voasiz.com</div>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
 
         <div className="card-holder">
-        <Card className="contact-card">
-          <Card.Body className="card-body">This is some text within a card body.</Card.Body>
-        </Card>
+          <Card className="contact-card">
+            <Card.Body>
+              <div className="card-body">
+                <div className="contact-icon">
+                  <AiOutlineHome />
+                </div>
+
+                <div className="contact-detail">
+                  <div className="row-list">
+                    <div className="row-item">VOASIZ (PVT) Ltd, N0 188/2,</div>
+                    <div className="row-item">Pahala Yagoda,</div>
+                    Ganemulla. Sri Lanka.
+                    <div className="row-item"></div>
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
 
         <div className="card-holder">
-        <Card className="contact-card">
-          <Card.Body className="card-body">This is some text within a card body.</Card.Body>
-        </Card>
+          <Card className="contact-card">
+            <Card.Body>
+              <div className="card-body">
+                <div className="contact-icon">
+                  <FiPhone />
+                </div>
+
+                <div className="contact-detail">
+                  <div className="row-list">
+                    <div className="row-item">+94 77 406 8783</div>
+                    <div className="row-item">+94 77 519 0167</div>
+                  </div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
         </div>
       </div>
     </section>
