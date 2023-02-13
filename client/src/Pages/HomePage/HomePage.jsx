ccd1d670705973db841d30fec32bbf634e1e6a2d
Shenal Akalanka <shenalakalanka513@gmail.com>
2023-02-13 15:23:22 +0530

    footer

--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import TopNavBar from '../../Components/NavBar/TopNavBar'
-import Header from '../../Components/Header/Header'
-import About from '../../Components/About Us/AboutUs'
-import Developer from '../../Components/Game Developer/GameDeveloper'
-import Customer from '../../Components/Customer/Customer'
-import Experience from '../../Components/Experience/Experience'
-import Contact from '../../Components/Contact/Contact'
+import React from "react";
+import TopNavBar from "../../Components/NavBar/TopNavBar";
+import Header from "../../Components/Header/Header";
+import About from "../../Components/About Us/AboutUs";
+import Developer from "../../Components/Game Developer/GameDeveloper";
+import Customer from "../../Components/Customer/Customer";
+import Experience from "../../Components/Experience/Experience";
+import Contact from "../../Components/Contact/Contact";
+import Footer from "../../Components/Footer/Footer";
 
 const HomePage = () => {
   return (
     <div>
-      <TopNavBar/>
-      <Header/>
-      <About/>
-      <Developer/>
-     <Customer/>
-     <Experience/>
-     <Contact/>
-
+      <TopNavBar />
+      <Header />
+      <About />
+      <Developer />
+      <Customer />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
