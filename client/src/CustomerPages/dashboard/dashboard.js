993d53dd8923d196ec15a8147b6913171c52bb5e
Nethmi-24 <batawalann.20@uom.lk>
2023-03-28 11:20:49 +0530

    dashboard changes and gamedetail changes

--- client/src/CustomerPages/dashboard/dashboard.js
+++ client/src/CustomerPages/dashboard/dashboard.js
@@ -5,14 +5,49 @@ import { PieChart, Pie,Line,LineChart, Sector, ResponsiveContainer, BarChart, Ba
 import { Routes,Route,NavLink,BrowserRouter } from 'react-router-dom';
 import { doc,onSnapshot, collection,query,orderBy,limit} from "firebase/firestore";
 import { Button, Table, TableHead,TableCell,TableRow,TableBody } from "@mui/material";
-//import Sidebar from "../Component/Sidebar";
-import TemporaryDrawer from "../navbar/sidebar";
+
+function CustomTable({ title, data, columns }) {
+  return (
+    <div>
+      <h3 className="heading">{title}</h3>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {columns.map((column) => (
+              <TableCell key={column.key} style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px",fontWeight: "bold" }} scope="col">{column.label}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item, index) => (
+            <TableRow key={item.id || index}>
+              {columns.map((column) => (
+                <TableCell key={column.key} style={{ padding: "5px", fontFamily: "Arial", fontSize: "10px" }}>{item[column.key]}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+  </div>
+  );
+}
+
+
 function Dashboard(){
 
   const [Addata, setAdData] = useState([]);
   const[trendingAd,setTrendingAdData]=useState([]);
+  const[trendingGame,setTrendingGameData]=useState([]);
   const AdCollectionRef = collection(db,"AdData_Collection");
+  const GameDataCollectionRef=collection(db,"GamesCollection");
   const q = query(AdCollectionRef, orderBy("AdViewCount", "desc"), limit(3));
+  const tg=query(GameDataCollectionRef,orderBy("Rank"),limit(3)); 
+
+  const dataset1 = [
+    { name: 'OngoingAds', value: 5 },
+    { name: 'RejectedAds', value: 2 },
+    { name: 'PendingAds', value: 2 },
+  ];
 
     useEffect(() => {
         onSnapshot(q,snapshot => {
@@ -25,11 +60,7 @@ function Dashboard(){
             }))
           })
     }, [])
-  const dataset1 = [
-    { name: 'OngoingAds', value: 5 },
-    { name: 'RejectedAds', value: 2 },
-    { name: 'PendingAds', value: 2 },
-  ];
+
   useEffect(() => {
     onSnapshot(AdCollectionRef,snapshot => {
         setAdData(snapshot.docs.map(doc => {
@@ -41,127 +72,73 @@ function Dashboard(){
         }))
       })
 }, [])
+useEffect(() => {
+  onSnapshot(tg,snapshot => {
+      setTrendingGameData(snapshot.docs.map(doc => {
+          return {
+              id: doc.id,
+              viewing: false,
+              ...doc.data()   //breaking individual fields
+          }
+      }))
+    })
+}, [])
   const dataset2 = Addata.map((item) => ({
      name: item.Adname,
      Adview:item.AdViewCount }));
 
-  //   const maxAdview = Math.max(...dataset2.map((item) => item.Adview)); // calculate max ad view count here
-  //const maxAdview = Math.max.apply(null, dataset2.map(item => item.Adview));
- 
      const data = [
     {"name": "1/March","uv": 4000,"pv": 2400,"amt": 2400},
     {"name": "2/March","uv": 3000,"pv": 1398,"amt": 2210},
     {"name": "3/March", "uv": 2000,"pv": 9800,"amt": 2290},
     {"name": "4/March","uv": 2780,"pv": 3908,"amt": 2000},
-    {"name": "5/March","uv": 1890,"pv": 4800,"amt": 2181},
-    {"name": "6/March","uv": 2390,"pv": 3800,"amt": 2500},
-    {"name": "7/March","uv": 3490,"pv": 4300,"amt": 2100},
-    {"name": "1/March","uv": 4000,"pv": 2400,"amt": 2400},
-    {"name": "2/March","uv": 3000,"pv": 1398,"amt": 2210},
-    {"name": "3/March", "uv": 2000,"pv": 9800,"amt": 2290},
-    {"name": "4/March","uv": 2780,"pv": 3908,"amt": 2000},
-    {"name": "5/March","uv": 1890,"pv": 4800,"amt": 2181},
-    {"name": "6/March","uv": 2390,"pv": 3800,"amt": 2500},
-    {"name": "7/March","uv": 3490,"pv": 4300,"amt": 2100}
+   
   ]
-  
-  const colors = ['#8884d8', '#82ca9d', '#ffc658'];
+   const colors = ['#8884d8', '#82ca9d', '#ffc658'];
+
   return (
-    <div className="App">
-      <div style={{display:"flex"}}>
+  <div className="App">
+    <div style={{display:"flex"}}>
       <div className="smallbox">
-      <div> <h3 style={{fontFamily: "Arial", fontSize: "10px", fontWeight: "bold"}}>Your Top 3 Ads</h3></div> 
-      <div>
-        <Table>
-          <TableHead>
-            <TableRow>
-            <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Advertisement Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
- 
-  {trendingAd.map((item, index) => (
-      <TableRow key={item.id || index}>
-        <TableCell style={{ padding: "5px",fontFamily: "Arial", fontSize: "10px" }}>{item.Adname}</TableCell>
-      </TableRow>
-    ))}
-      
-      </TableBody>
-        </Table>
+        <CustomTable
+                title="Your Top 3 Ads"
+                data={trendingAd}
+                columns={[    { key: 'Adname', label: 'Advertisement Name' },  ]}/>
         <div>
           <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/Profile'>MoreInfo</NavLink></Button>
         </div>
-      </div> 
-</div>
-<div className="smallbox">
-      <div> <h3 style={{fontFamily: "Arial", fontSize: "10px", fontWeight: "bold"}}>Your Top 3 Ads</h3></div> 
-      <div>
-        <Table>
-          <TableHead>
-            <TableRow>
-            <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Advertisement Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
- 
-  {trendingAd.map((item, index) => (
-      <TableRow key={item.id || index}>
-        <TableCell style={{ padding: "5px",fontFamily: "Arial", fontSize: "10px" }}>{item.Adname}</TableCell>
-      </TableRow>
-    ))}
-      
-      </TableBody>
-        </Table>
-        <div>
-          <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/Profile'>MoreInfo</NavLink></Button>
+     </div>
+    <div className="smallbox">
+
+        <CustomTable
+                title="Top 3 Games Today"
+                data={trendingGame}
+                columns={[    { key: 'Name', label: 'Game Name' },  ]}/>
+           <div>
+          <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/Tables'>MoreInfo</NavLink></Button>
         </div>
-      </div> 
-</div>
-<div className="smallbox">
-      <div> <h3 style={{fontFamily: "Arial", fontSize: "10px", fontWeight: "bold"}}>Your Top 3 Ads</h3></div> 
+    </div>
+      
+    <div className="smallbox">
+         <div> <h3 className="heading">Your Ads</h3></div> 
       <div>
         <Table>
           <TableHead>
             <TableRow>
-            <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Advertisement Name</TableCell>
+            <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Summary</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
  
-  {trendingAd.map((item, index) => (
-      <TableRow key={item.id || index}>
-        <TableCell style={{ padding: "5px",fontFamily: "Arial", fontSize: "10px" }}>{item.Adname}</TableCell>
-      </TableRow>
-    ))}
-      
-      </TableBody>
-        </Table>
-        <div>
-          <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/Profile'>MoreInfo</NavLink></Button>
-        </div>
-      </div> 
-</div>
-  <div className="smallbox">
-      <div> <h3 style={{fontFamily: "Arial", fontSize: "10px", fontWeight: "bold"}}>Your Top 3 Ads</h3></div> 
-      <div>
-        <Table>
-          <TableHead>
-            <TableRow>
-            <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Advertisement Name</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
  
-  {trendingAd.map((item, index) => (
-      <TableRow key={item.id || index}>
-        <TableCell style={{ padding: "5px",fontFamily: "Arial", fontSize: "10px" }}>{item.Adname}</TableCell>
-      </TableRow>
-    ))}
+  <TableRow> <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>On Going Ads</TableCell><TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>5</TableCell> </TableRow>
+  <TableRow><TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Pending Ads</TableCell><TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>4</TableCell></TableRow>
+ <TableRow> <TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>Rejected Ads</TableCell><TableCell style={{ padding: "3px", fontFamily: "Arial", fontSize: "10px", fontWeight: "bold" }}>1</TableCell></TableRow>
       
       </TableBody>
         </Table>
         <div>
-          <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/Profile'>MoreInfo</NavLink></Button>
+          <Button style={{ fontSize:'9px',backgroundColor:"rgb(0, 138, 197)",color:"white",marginLeft:"160px",marginTop:"3px" }}><NavLink to='/viewAd'>MoreInfo</NavLink></Button>
         </div>
       </div> 
 </div>
--- client/src/CustomerPages/table/table.js
+++ client/src/CustomerPages/table/table.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { db } from "../../Custfirebase.config"
 import { useEffect, useState } from 'react';
-import { orderBy,query } from "firebase/firestore";
+import { orderBy,query, where } from "firebase/firestore";
 import { doc,onSnapshot, collection, limit } from "firebase/firestore" 
 import './tableStyle.css'
 import { Button } from "@mui/material";
@@ -39,18 +39,29 @@ function MyTable({ filteredGame }) {
 const Tables=() =>{
 const [data, setData] = useState([]);
 const[tgames,settgames]=useState([]);
+const[multiplayergames,setmultiplayerGames]=useState([]);
 const gamesCollectionRef = collection(db,"GamesCollection");
 const q = query(gamesCollectionRef, orderBy("Rank"));
 const tg =query (gamesCollectionRef,orderBy("Rank"),limit(2));
+const mpg=query (gamesCollectionRef, where ("game_type_id","=="," N3VYG3mWarjLB3J71sZN"));
+
 const [AllGamesTable, setAllGamesTable] = useState(true);
 const handleAllGamesTable = () => {
 setAllGamesTable(true);
 setTrendingGamesTable(false);
+setmultiplayerGamesTable(false);
 };
 const [TrendingGamesTable, setTrendingGamesTable] = useState(false);
 const handleTrendingGamesTable = () => {
   setTrendingGamesTable(true);
   setAllGamesTable(false);
+  setmultiplayerGamesTable(false);
+};
+const [MultiPlayerGamesTable, setmultiplayerGamesTable] = useState(false);
+const handleMultiplayerGamesTable = () => {
+  setTrendingGamesTable(false);
+  setAllGamesTable(false);
+  setmultiplayerGamesTable(true);
 };
 
   useEffect(() => {
@@ -75,19 +86,31 @@ const handleTrendingGamesTable = () => {
         }))
       })
 }, [])
+useEffect(()=> {
+  onSnapshot(mpg,snapshot => {
+    setmultiplayerGames(snapshot.docs.map(doc=>{
+      return{
+        id:doc.id,
+        viewing:false,
+        ...doc.data()
+      }
+    }))
+  })
+},[])
  return(
     <div className='ListContainer'> 
     <div className='ListTitle'>Game Details</div>
     <div style={{ display: 'flex', justifyContent: 'space-between',margin:"20px"}}>
       <Button variant="contained"  onClick={handleAllGamesTable} >All Games</Button>
       <Button variant="contained"  onClick={handleTrendingGamesTable}>Trending Games</Button>
-      <Button variant="contained"  >Multi Player Games</Button>
+      <Button variant="contained" onClick={handleMultiplayerGamesTable} >Multi Player Games</Button>
       <Button variant="contained"  >Single Player Games</Button>
       <Button variant="contained"  >Adventure Player Games</Button>
       </div>
       <TableContainer component={Paper} classname="table">
       {AllGamesTable && <MyTable filteredGame={data} />}
       {TrendingGamesTable && <MyTable filteredGame={tgames} />}
+      {MultiPlayerGamesTable&&<MyTable filteredGame={multiplayergames}/>}
     </TableContainer>
       </div> 
     )
